631c7475c114944c5f44ec3dac5866277b630de2
Jamie Zhang <akjamie.zhang@outlook.com>
2020-12-02 22:33:19 +0800

    GCP appengine standard

--- src/main/java/org/akj/springboot/controller/HelloWorldController.java
+++ src/main/java/org/akj/springboot/controller/HelloWorldController.java
@@ -1,17 +1,22 @@
 package org.akj.springboot.controller;
 
 import org.akj.springboot.utils.IpUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloWorldController {
+    @Value("${spring.application.name}")
+    private String appName;
 
     @GetMapping("/hello-world")
     public ResponseEntity hello(HttpServletRequest request) {
@@ -24,4 +29,18 @@ public class HelloWorldController {
 
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(map);
     }
+
+    @GetMapping("/hello-world/info")
+    public ResponseEntity helloWithInfo(HttpServletRequest request) {
+        String sourceIp = IpUtil.getIpAddr(request);
+        String localIp = IpUtil.getLocalIpAddress();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("sourceIp",sourceIp);
+        map.put("targetIp",localIp);
+        map.put("application", Map.of("name", appName, "version", "2", "server","GCP appEngine standard"));
+
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(map);
+    }
+
+
 }
